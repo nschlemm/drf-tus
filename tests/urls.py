f0c220171ef30b5536a7f86997d640e1f0bbc13e
Nikolaus Schlemm <ns@lugensa.com>
2020-06-11 14:43:57 +0200

    avoid ImproperlyConfiguredException:
    django.core.exceptions.ImproperlyConfigured: Specifying a namespace in include() without providing an app_name is not supported. Set the app_name attribute in the included module, or pass a 2-tuple containing the list of patterns and app_name instead.

--- tests/urls.py
+++ tests/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls import url, include
 from rest_framework_tus.urls import urlpatterns as rest_framework_tus_urls
 
 urlpatterns = [
-    url(r'^', include(rest_framework_tus_urls, namespace='rest_framework_tus')),
+    url(r'^', include((rest_framework_tus_urls, 'rest_framework_tus'), namespace='rest_framework_tus')),
 ]
